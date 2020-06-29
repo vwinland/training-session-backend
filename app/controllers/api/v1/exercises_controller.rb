28fc4d8bc2f411ae7cff5a7599342cc4bb8a8bd5
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 14:10:37 -0400

    adds CRUD functaionality to excercise controller

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,6 +1,33 @@
 class Api::V1::ExercisesController < ApplicationController
 
     def index 
-
+        @exercises = exercise.all 
+        render json: @exercises
     end
+
+    def create
+        @workout = Workout.new(exercise_params)
+        if @workout.save 
+            render json: @account
+        else
+            render json: {error: 'Error Adding Working'}
+    end 
+
+    def show
+        @exercise = exercise.find(params[:id]) #refactor
+        render json: @exercise
+    end
+
+    def destroy
+        @exercise = exercise.find(params[:id]) #refactor 
+        @exercise.destory
+    end
+
+    private 
+
+    def exercise_params 
+        params.require(:exercise).permit(:name, :category)
+    end
+    
+
 end
