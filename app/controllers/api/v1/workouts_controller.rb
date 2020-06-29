eca31897adad873a7cd365816c72b2ee8a9d1adb
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 14:06:03 -0400

    updates index and create methods. adds error messaging if workout is created without workout params

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::WorkoutsController < ApplicationController
 
     def index 
-
+        @workouts = Workout.all 
+        render json: @workouts
     end
 
     def create
-
+        @workout = Workout.new(workout_params)
+        if @workout.save 
+            render json: @account
+        else
+            render json: {error: 'Error Adding Working'}
     end 
 
     def show
