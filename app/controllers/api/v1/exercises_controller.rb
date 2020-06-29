49e6982f2ebc5bdbbd7bf408935e88b1981a4fb2
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 14:18:04 -0400

    creates before action to set_workout to render correct workout_id and exercise

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,32 +1,32 @@
 class Api::V1::ExercisesController < ApplicationController
 
+    before_action :set_workout
+
     def index 
-        @exercises = exercise.all 
+        @workout.exercises
         render json: @exercises
     end
 
     def create
-        @workout = Workout.new(exercise_params)
-        if @workout.save 
-            render json: @account
-        else
-            render json: {error: 'Error Adding Working'}
+     
     end 
 
     def show
-        @exercise = exercise.find(params[:id]) #refactor
-        render json: @exercise
+      
     end
 
     def destroy
-        @exercise = exercise.find(params[:id]) #refactor 
-        @exercise.destory
+       
     end
 
     private 
 
+    def set_workout
+
+    end
+
     def exercise_params 
-        params.require(:exercise).permit(:name, :category)
+      
     end
     
 
