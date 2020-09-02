91ced1c01f178783de5edb78497587b2e3306934
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-02 16:42:02 -0400

    add bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index' do 
+end
   namespace :api do 
     namespace :v1 do 
+ 
       resources :workouts do
         resources :exercises
       end
