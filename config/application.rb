98429d71a9ec9b9ddaeb26e611f979aa55a91558
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-02 17:48:37 -0400

    fix action mailer bug that crashed request

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module TrainingSessionBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_controller.default_protect_from_forgery = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
+
 Rails.application.routes.draw do
-
   namespace :api do 
-    namespace :v1 do 
-      resources :users, only: [:create, :show, :workouts]
+    namespace :v1 do
 
- 
-      resources :workouts do
+     
+        resources :workouts do
         resources :exercises
+        end
       end
     end
-  end
+
 
 end
 
