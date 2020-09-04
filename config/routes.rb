4b36c150ef2d901d10927edabd2be9acb4876b84
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-03 21:18:24 -0400

    update routes for signup/signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 
 Rails.application.routes.draw do
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+
+  get "/api/v1/current_user_workouts, to: "api/v1/workouts#current_user_workouts"
   namespace :api do 
     namespace :v1 do
-
- 
+      resources :users do 
         resources :workouts do
         resources :exercises
         end
